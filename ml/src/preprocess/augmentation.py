af6f87be37978489628728c0d79bfbbc40f45985
Sanasaifi786 <sanasaifi1420@gmail.com>
2026-09-07 22:27:59 +0530

    feat: add mixup augmentation utility and one-hot label conversion functions

--- ml/src/preprocess/augmentation.py
+++ ml/src/preprocess/augmentation.py
@@ -13,3 +13,19 @@ def to_one_hot(label,num_classes = 7):
     if not isinstance(label, torch.Tensor):
         label = torch.tensor(label, dtype=torch.long)
     return F.one_hot(label, num_classes=num_classes).float()
+
+def mixup(image1,label1,image2, label2, alpha = 0.4,num_classes = 7):
+    
+    if alpha > 0:
+        lam = float(torch.distributions.Beta(alpha,alpha).sample().item)
+    else:
+        lam = 1.0
+    
+    mixed_image = lam * image1 + (1.0 - lam) * image2
+
+    l1_onehot = to_one_hot(label1, num_classes = num_classes).to(image1.device)
+    l2_onehot = to_one_hot(label2, num_classes = num_classes).to(image2.device)
+
+    mixed_label = lam * l1_onehot + (1.0 - lam) * l2_onehot
+
+    return mixed_image, mixed_label
